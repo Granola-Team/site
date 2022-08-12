f5cd65985405b15696268e42cacc0a72ccf2f274
jstavh <jstavh@gmail.com>
2022-08-12 17:27:14 -0400

    added tailwind and css but trying to integrate them

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), partytown()]
+});
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// class="text=5xl text-white"
+// class="text-red-200"
+// class="bg-red"
+
+const Header = () => {
+    return (
+        <header class="bg-red text white">
+            <div class="bg-red text white flex justfy-between items-center">
+                <div class="bg-red text white">
+                    Granola
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header; 
