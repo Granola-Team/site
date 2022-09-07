62c6204d0e174cdd544d39cce76303bd173804c2
Jason Hoffman <106464930+jstavh@users.noreply.github.com>
2022-09-07 15:19:45 -0400

    Re-do home page with new logo (#10)
    * re-do homepage and integrate the new logo
    
    * need to push up new image in public folder which is one directory below
    
    * ignoring package-lock.json
    
    * it's a file so assuming no slash at the end needed
    
    * seeing if logo appears in preview now

--- src/astro/src/components/Hero.jsx
+++ src/astro/src/components/Hero.jsx
@@ -1,16 +1,20 @@
 import Nav from './Nav'
 import '../styles/hero.scss'
+import '../../public/images/GranolaLogo.png'
 
 const Hero = () => {
     return (
         <section className='hero'>
+            <div className='containerWords'>
+                <h1><b><span style={{fontSize: "150%"}}>We make winning software teams.</span></b></h1>
+                <p style={{fontWeight: "lighter", marginBottom: "-1em", marginTop: "2em"}}>Granola is a software consultancy.</p>
+                <p style={{fontWeight: "lighter"}}>We make winning teams using our expertise in leadership, DevOps, Web3, distributed systems, 
+                    functional programming, and data engineering.</p>
+                <br/><br/>
+                <p style={{fontWeight: "lighter"}}>Say hello: <a href="mailto:hello@granola.team">hello@granola.team</a></p>
+            </div>
             <div className='container'>
-                <header>
-                    <h1>Granola</h1>
-                    <p>We make winning software teams.</p>
-                    <p>Granola is a software consultancy. We make winning teams using our expertise in leadership, DevOps, Web3, distributed systems, functional programming, and data engineering.</p>
-                    <p>Say hello: <a href="mailto:hello@granola.team">hello@granola.team</a></p>
-                </header>
+                <img src="../../images/GranolaLogo.png" />
             </div>
         </section>
     )
