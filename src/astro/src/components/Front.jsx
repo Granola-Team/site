aea5ce176fd03cc98d9631c1ec72018b791fd709
jstavh <jstavh@gmail.com>
2022-09-16 16:15:53 -0400

    gave semi transparent background to contact info on home page

--- src/astro/src/components/Front.jsx
+++ src/astro/src/components/Front.jsx
@@ -5,12 +5,13 @@ const Front = () => {
     return (
         <section className="flex items-center">
             <div className="flex flex-col h-screen px-12">
-                <h1 className="mt-24 h-10 text-5xl text-white"><b><span>We make winning software teams.</span></b></h1>
+                    <h1 className="mt-24 h-10 text-5xl text-white"><b>
+                        <span className="z-50">We make winning software teams.</span></b></h1>
                 <p className="text-3xl text-white">Granola is a software consultancy.</p>
                 <p className="-mt-9 flex-grow text-3xl text-white">We make winning teams using our expertise in leadership, DevOps, Web3, distributed systems, 
                     functional programming, and data engineering.</p>
-                <p className="mb-48 text-3xl text-white">Say hello: 
-                    <a href="mailto:hello@granola.team" className="mb-0 text-3xl text-logoOrange">
+                <p className="mb-48 text-3xl text-white bg-emerald-500 bg-opacity-10">Say hello: 
+                    <a href="mailto:hello@granola.team" className="mb-0 text-3xl text-logoOrange bg-emerald-500 bg-opacity-20">
                         hello@granola.team
                     </a></p>
             </div>
