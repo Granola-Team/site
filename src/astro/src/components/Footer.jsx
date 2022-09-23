2f727489ad56c8a283b441591c02a160da9cbfa1
jstavh <jstavh@gmail.com>
2022-09-23 17:36:10 -0400

    afternoon changes

--- src/astro/src/components/Footer.jsx
+++ src/astro/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import '../styles/styles.css';
 
 const Footer = () => { 
     return (
-        <footer className="text-2xl text-white text-center">
-            <h6 className="m-0 p-2">Copyright © 2022 Granola Systems. All rights reserved.</h6>
+        <footer className="text-xs text-white text-center">
+            <p className="m-0 p-2">Copyright © 2022 Granola Systems. All rights reserved.</p>
         </footer>
     )
 }
--- src/astro/src/components/Front.jsx
+++ src/astro/src/components/Front.jsx
@@ -7,9 +7,9 @@ const Front = () => {
             <div className="flex flex-col px-24 xl:h-screen">
                     <h1 className="mt-36 h-10 text-5xl text-white"><b>
                         <span>Winning Software Teams</span></b></h1>
-                <p className="xl:flex-grow text-3xl text-white">Granola is a software consultancy. We make winning teams using our expertise 
+                <p className="pt-12 sm:p-0 xl:flex-grow text-3xl text-white">Granola is a software consultancy. We make winning teams using our expertise 
                     in leadership, DevOps, Web3, distributed systems, functional programming, and data engineering.</p>
-                <p className="mb-72 text-3xl text-white">Reach out:&nbsp;
+                <p className="mb-72 text-3xl text-white">
                     <a href="mailto:hello@granola.team" className="mb-0 text-3xl text-logoOrange z-20">
                         hello@granola.team
                     </a></p>
