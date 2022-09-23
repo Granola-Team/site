06945529ecf6a46cdecddcfcafab4dc7b30e015f
jstavh <jstavh@gmail.com>
2022-09-23 17:49:37 -0400

    need check mobile view

--- src/astro/src/components/Nav.jsx
+++ src/astro/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import '../../public/images/GranolaHorizontalDarkBg.png'
  
 const Nav = () => {
     return (
-        <div className="border-b-[0.03rem] border-solid border-white border-t-0 border-x-0">
+        <div className="border-b-[0.03rem] border-solid border-white border-t-0 border-x-0 overflow-auto">
             <nav className="flex items-center gap-6">
                 <a href="/" ><img className="h-20 w-60" src="../../images/GranolaHorizontalDarkBg.png" /></a>
                 <a href="/careers" className="text-2xl text-white no-underline">Careers</a>
