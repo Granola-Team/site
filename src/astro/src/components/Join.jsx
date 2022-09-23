765a8a9dcbe81a6d5c86866d4c683ccca3838ce8
jstavh <jstavh@gmail.com>
2022-09-23 16:39:03 -0400

    quick fix

--- src/astro/src/components/Join.jsx
+++ src/astro/src/components/Join.jsx
@@ -1,12 +1,12 @@
 import '../styles/styles.css';
-import * as React from 'react';
-import { useLocation } from 'react-router-dom';
+// import * as React from 'react';
+// import { useLocation } from 'react-router-dom';
 
 const Join = () => { 
-    const location = useLocation()
+// const location = useLocation()
 
     return (
-        <div>{location.pathname !=="/careers" && 
+        <div>{ window.location.href !== "http://localhost:3000/careers/" && 
             <div className="mt-10 mb-10">
                 <a href="mailto:hello@granola.team" className="py-4 px-8 rounded-lg text-yellow-300 no-underline text-2md 
                 bg-slate-500">Reach out</a>
