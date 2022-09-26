f0511a31ee85897cbda8c5f56add151b4e7eb7d0
jstavh <jstavh@gmail.com>
2022-09-26 18:45:09 -0400

    trying to incorporate a bottom only footer

--- src/astro/src/components/Footer.jsx
+++ src/astro/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import '../styles/styles.css';
 
 const Footer = () => { 
     return (
-        <footer className="absolute inset-x-0 bottom-0 text-center">
+        <footer className="fixed inset-x-0 bottom-0 text-center">
             <p className="text-xs text-white">Copyright © 2022 Granola Systems. All rights reserved.</p>
         </footer>
     )
