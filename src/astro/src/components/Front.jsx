c662622a53fc407137e090549ea68d811d12b79f
jstavh <jstavh@gmail.com>
2022-09-26 11:33:42 -0400

    home page, separating concerns

--- src/astro/src/components/Front.jsx
+++ src/astro/src/components/Front.jsx
@@ -7,9 +7,9 @@ const Front = () => {
             <div className="flex flex-col px-24 xl:h-screen">
                     <h1 className="mt-36 h-10 text-4xl sm:text-5xl text-white"><b>
                         <span>Winning Software Teams</span></b></h1>
-                <p className="pt-12 sm:p-0 xl:flex-grow text-2xl sm:text-3xl text-white">Granola is a software consultancy. We make winning teams using our expertise 
+                <p className="mt-12 pt-12 sm:p-0 xl:flex-grow text-2xl sm:text-3xl text-white">Granola is a software consultancy. We make winning teams using our expertise 
                     in leadership, DevOps, Web3, distributed systems, functional programming, and data engineering.</p>
-                <p className="mb-72 text-2xl sm:text-3xl text-white">
+                <p className="mb-96 text-2xl sm:text-3xl text-white">
                     <a href="mailto:hello@granola.team" className="mb-0 text-2xl sm:text-3xl text-logoOrange z-20">
                         hello@granola.team
                     </a></p>
--- src/astro/src/components/Nav.jsx
+++ src/astro/src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <div className="border-b-[0.03rem] border-solid border-white border-t-0 border-x-0 overflow-auto">
             <nav className="flex items-center gap-6">
                 <a href="/" ><img className="h-[3rem] w-[9rem] sm:h-20 sm:w-60" src="../../images/GranolaHorizontalDarkBg.png" /></a>
-                <a href="/careers" className="text-xl sm:text-2xl text-white no-underline">Careers</a>
+                <a href="/" className="text-xl sm:text-2xl text-white no-underline">Careers</a>
                 <a href="mailto:hello@granola.team" className="text-xl sm:text-2xl text-white no-underline">Contact</a>
             </nav>       
         </div>
