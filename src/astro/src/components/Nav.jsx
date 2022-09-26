5b738260fce11f6a3120a9507ae42fd28a8352db
Robin Bate Boerop <robin@granola.team>
2022-09-26 21:58:56 +0000

    Tweak home page

--- src/astro/src/components/Nav.jsx
+++ src/astro/src/components/Nav.jsx
@@ -1,11 +1,15 @@
+import '../styles/styles.css';
+import '../../public/images/GranolaHorizontalDarkBg.png'
+ 
 const Nav = () => {
     return (
-        <ul>
-            <li>About</li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>Contact</li>
-        </ul>
+        <div className="border-b-[0.03rem] border-solid border-white border-t-0 border-x-0 overflow-auto">
+            <nav className="flex items-center gap-6">
+                <a href="/" ><img className="h-[3rem] w-[9rem] sm:h-20 sm:w-60" src="../../images/GranolaHorizontalDarkBg.png" /></a>
+                <a href="/" className="text-xl sm:text-2xl text-white no-underline">Careers</a>
+                <a href="mailto:hello@granola.team" className="text-xl sm:text-2xl text-white no-underline">Contact</a>
+            </nav>       
+        </div>
     )
 }
 
