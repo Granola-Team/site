2e3835cddbf74fb53a5e5b3e1cbcefb4788ac377
jstavh <jstavh@gmail.com>
2022-09-27 13:29:40 -0400

    attempting button final

--- src/astro/src/components/Join.jsx
+++ src/astro/src/components/Join.jsx
@@ -1,15 +1,23 @@
 import '../styles/styles.css';
-// import * as React from 'react';
-// import { useLocation } from 'react-router-dom';
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Join = () => { 
-// const location = useLocation()
+
+    const location = useLocation();
+
+    if (typeof window !== "undefined") {
+    return <React.Fragment />
+    }
 
     return (
+        <div>{location.pathname !== "/careers" && 
             <div className="mb-10">
                 <a href="mailto:hello@granola.team" className="py-4 px-8 rounded-lg text-yellow-300 no-underline text-2md 
                 bg-slate-500">Join our team!</a>
             </div>
+            }
+        </div>
     )
 }
 
