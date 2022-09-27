3fc56c3f3df3d3498a38e2566515ab80fcb26203
jstavh <jstavh@gmail.com>
2022-09-27 16:45:56 -0400

    saving progress

--- src/astro/src/components/Join.jsx
+++ src/astro/src/components/Join.jsx
@@ -1,22 +1,21 @@
 import '../styles/styles.css';
-import * as React from 'react';
-import { useLocation } from 'react-router-dom';
+// import * as React from 'react';
+// import { useLocation } from 'react-router-dom';
 
 const Join = () => { 
-
+/*
     const location = useLocation();
-
+    {location.pathname !== "/careers" && 
     if (typeof window !== "undefined") {
     return <React.Fragment />
     }
-
+*/
     return (
-        <div>{location.pathname !== "/careers" && 
+        <div>
             <div className="mb-10">
                 <a href="mailto:hello@granola.team" className="py-4 px-8 rounded-lg text-yellow-300 no-underline text-2md 
                 bg-slate-500">Join our team!</a>
             </div>
-            }
         </div>
     )
 }
