ebacafb40dbede3eb1afecf7aa73eb1486d82242
Jason Hoffman <106464930+jstavh@users.noreply.github.com>
2022-09-29 16:52:50 -0400

    career page, jobs pages, and added mdx integration so no more legacy (#22)
    * career page, jobs pages, and added mdx integration so no more legacy
    * simplified careers page
    * renamed jobs pages to their respective titles

--- src/astro/astro.config.mjs
+++ src/astro/astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from '@astrojs/tailwind';
 import partytown from "@astrojs/partytown";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), partytown()],
+  integrations: [tailwind(), react(), partytown(), mdx()],
 });
