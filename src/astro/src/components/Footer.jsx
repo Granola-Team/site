4b3a9e692a1312a951afc091f22034fa7ddd23a2
jstavh <jstavh@gmail.com>
2022-09-29 16:22:31 -0400

    footer works and is always at the bottom no matter position in scrolling or screen size

---
+++ src/astro/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../styles/styles.css';
+
+const Footer = () => { 
+    return (
+        <footer>
+            <p className="text-center text-xs text-white">Copyright © 2022 Granola Systems. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
