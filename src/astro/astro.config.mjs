d262ea7f9506411202a0fd21bad57c87b9e41ef9
Robin Bate Boerop <me@robinbb.com>
2022-10-05 19:05:05 -0300

    Correct and improve the build and deployment process (#27)
    * Add netlify.toml to define build
    
    * Remove unnecessary images
    
    * Remove partytown dependency
    
    * Tweak footer

--- src/astro/astro.config.mjs
+++ src/astro/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from '@astrojs/tailwind';
-import partytown from "@astrojs/partytown";
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), partytown(), mdx()],
+  integrations: [tailwind(), react(), mdx()],
 });
--- src/astro/src/components/Footer.jsx
+++ src/astro/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import '../styles/styles.css';
 
 const Footer = () => { 
     return (
-        <p className="text-center text-xs text-white">Copyright © 2022 Granola Systems. All rights reserved.</p>
+        <p className="text-center text-xs text-white">Copyright 2022 Granola Systems Inc. All rights reserved.</p>
     )
 }
 
