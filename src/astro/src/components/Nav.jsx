4d764000df6f234d8c45e966c37a6b9afd159c1a
jstavh <jstavh@gmail.com>
2022-10-07 00:04:46 -0400

    nav-bar logo on the left while the rest is on the right

--- src/astro/src/components/Nav.jsx
+++ src/astro/src/components/Nav.jsx
@@ -3,13 +3,19 @@ import '../../public/images/GranolaHorizontalDarkBg.png'
 
 const Nav = () => {
     return (
-        <div className="border-b-[0.03rem] border-solid border-white border-t-0 border-x-0 overflow-auto">
-            <nav className="flex items-center gap-6">
-                <a href="/" ><img className="h-[3rem] w-[9rem] sm:h-20 sm:w-60" src="../../images/GranolaHorizontalDarkBg.png" /></a>
-                <a href="/careers" className="text-xl sm:text-2xl text-white no-underline">Careers</a>
-                <a href="mailto:hello@granola.team" className="text-xl sm:text-2xl text-white no-underline">Contact</a>
-            </nav>       
-        </div>
+        <nav className="flex justify-between border-b-[0.03rem] border-solid 
+            border-white border-t-0 border-x-0 overflow-auto">
+                <div className="flex justify-start">
+                    <a href="/" ><img className="h-[3rem] w-[9rem] sm:h-20 sm:w-60" 
+                        src="../../images/GranolaHorizontalDarkBg.png" /></a>
+                </div>
+                <div className="flex justify-end items-center mr-6 gap-6">
+                    <a href="/careers" 
+                        className="text-xl sm:text-2xl text-white no-underline">Careers</a>
+                    <a href="mailto:hello@granola.team" 
+                        className="text-xl sm:text-2xl text-white no-underline">Contact</a>
+                </div>
+        </nav>
     )
 }
 
