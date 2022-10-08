50f937778c33b2c4922a6f363bcc7e885459f58d
jstavh <jstavh@gmail.com>
2022-10-08 13:24:28 -0400

    trying using a wrapper, clear-both, position absolute, etc

--- src/astro/src/components/Footer.jsx
+++ src/astro/src/components/Footer.jsx
@@ -2,7 +2,14 @@ import '../styles/styles.css';
 
 const Footer = () => { 
     return (
-        <p className="text-center text-xs text-white">Copyright 2022 Granola Systems Inc. All rights reserved.</p>
+        <div className="footer">
+            <div className="wrapper">
+                <p className="clear-both text-center text-xs text-white">
+                    Copyright 2022 Granola Systems Inc. All rights reserved.
+                </p>
+                <div className="push"></div>
+            </div>
+        </div>
     )
 }
 
