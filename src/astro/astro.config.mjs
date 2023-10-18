b28da5138645deafd0bfd8fbcb0208f5ed5007ea
jstavh <jstavh@gmail.com>
2023-10-18 14:55:47 -0700

    adjust files to match remove-react-PR-#60 and created blog button on home page

--- src/astro/astro.config.mjs
+++ src/astro/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), mdx()],
 });
