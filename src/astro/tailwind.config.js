285754ad82361512a15adaf46036f3e50c6414bf
jstavh <jstavh@gmail.com>
2023-10-20 14:49:10 -0700

    font change

--- src/astro/tailwind.config.js
+++ src/astro/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         xl: "1200px",
       },
       fontFamily: {
-        body: ['Open Sans', 'sans-serif'],
+        body: ['Merriweather', 'serif'],
       },
       colors: {
         logoOrange: {
